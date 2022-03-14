ab8dba7df19cd8b259afec8ccd8d7cdbcbf71eb7
kaining <xkn1994@gmail.cin>
2022-03-14 23:45:29 +0100

    #green BankAccountServiceTest passed

--- src/main/java/com/exalt/katabank/application/port/service/BankAccountService.java
+++ src/main/java/com/exalt/katabank/application/port/service/BankAccountService.java
@@ -4,16 +4,18 @@ import com.exalt.katabank.application.port.in.CheckTransactionsUseCase;
 import com.exalt.katabank.application.port.in.DepositMoneyUseCase;
 import com.exalt.katabank.application.port.in.RetrieveMoneyUseCase;
 import com.exalt.katabank.application.port.out.AccountPersistencePort;
+import com.exalt.katabank.domain.BankAccount;
 import com.exalt.katabank.domain.Money;
 import com.exalt.katabank.domain.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Service
 public class BankAccountService implements CheckTransactionsUseCase, DepositMoneyUseCase, RetrieveMoneyUseCase {
-
 
     private final AccountPersistencePort accountPersistencePort;
 
@@ -23,18 +25,33 @@ public class BankAccountService implements CheckTransactionsUseCase, DepositMone
     }
 
     @Override
-    public Set<Transaction> checkTransactions(Long accountId) {
-
-        return null;
+    public Set<Transaction> checkTransactions( Long accountId) {
+        Assert.notNull(accountId,"accountId is undefined");
+        BankAccount bankAccount = accountPersistencePort.loadAccount(accountId);
+        return new HashSet<>(bankAccount.getTransactions());
     }
 
     @Override
-    public boolean depositMoney(Long accountId, Money money) {
+    public boolean depositMoney(Long accountId,Money money) {
+        Assert.notNull(accountId,"accountId is undefined");
+        Assert.notNull(accountId,"money is undefined");
+        BankAccount bankAccount = accountPersistencePort.loadAccount(accountId);
+        if (bankAccount.deposit(money)){
+            accountPersistencePort.updateAccount(bankAccount);
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean retrieveMoney(Long accountId, Money money) {
+    public boolean retrieveMoney( Long accountId, Money money) {
+        Assert.notNull(accountId,"accountId is undefined");
+        Assert.notNull(accountId,"money is undefined");
+        BankAccount bankAccount = accountPersistencePort.loadAccount(accountId);
+        if (bankAccount.withDraw(money)){
+            accountPersistencePort.updateAccount(bankAccount);
+            return true;
+        }
         return false;
     }
 }
--- src/test/java/com/exalt/katabank/application/BankAccountServiceTest.java
+++ src/test/java/com/exalt/katabank/application/BankAccountServiceTest.java
@@ -4,16 +4,23 @@ import com.exalt.katabank.adapter.port.out.entity.BankAccountEntity;
 import com.exalt.katabank.adapter.port.out.entity.TransactionEntity;
 import com.exalt.katabank.application.port.out.AccountPersistencePort;
 import com.exalt.katabank.application.port.service.BankAccountService;
+import com.exalt.katabank.domain.BankAccount;
 import com.exalt.katabank.domain.Money;
 import static  org.junit.jupiter.api.Assertions.*;
+
+import com.exalt.katabank.domain.Transaction;
+import com.exalt.katabank.domain.TransactionType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class BankAccountServiceTest {
@@ -25,31 +32,29 @@ public class BankAccountServiceTest {
     @InjectMocks
     private BankAccountService bankAccountService;
 
+    BankAccount bankAccount;
     @BeforeEach
     void setup(){
-        TransactionEntity transactionEntity = new TransactionEntity();
-        transactionEntity.setBalance("100");
-        transactionEntity.setDateTime(LocalDateTime.now());
-        transactionEntity.setAmount("10");
-        transactionEntity.setType("DEPOSIT");
-        BankAccountEntity bankAccountEntity = new BankAccountEntity();
-        bankAccountEntity.setBalance("100");
-        bankAccountEntity.addTransaction(transactionEntity);
+        bankAccount = new BankAccount(1L,
+                new Money("100"), new ArrayList<>(List.of(new Transaction(1L,LocalDateTime.now(),new Money("10"), TransactionType.WITHDRAW,new Money("100")))) );
+
     }
 
     @Test
     void  when_deposit_money_the_balance_then_should_return_true(){
+        Mockito.when(accountPersistencePort.loadAccount(1L)).thenReturn(bankAccount);
         assertTrue(bankAccountService.depositMoney(1L,new Money("100")));
     }
 
     @Test
     void  when_retrieve_money_the_balance_then_should_true(){
+        Mockito.when(accountPersistencePort.loadAccount(1L)).thenReturn(bankAccount);
         assertTrue(bankAccountService.retrieveMoney(1L,new Money("100")));
-
     }
 
     @Test
     void  when_check_transaction_then_should_return_items(){
+        Mockito.when(accountPersistencePort.loadAccount(1L)).thenReturn(bankAccount);
         assertNotNull(bankAccountService.checkTransactions(1L));
     }
 }
